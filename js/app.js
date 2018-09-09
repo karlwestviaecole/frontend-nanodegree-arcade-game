9830091d41e9330d72e0457c929f965b6e561537
Karl West <karl.west@viaecole.se>
2018-09-09 12:15:13 +0200

    Added player and enemies

--- js/app.js
+++ js/app.js
@@ -1,40 +1,74 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+class Enemy {
+    
+    constructor() {
+        this.x = getRandomInt(-300, -100);
+        this.y = enemyLanes[getRandomInt(0, enemyLanes.length)];
+        this.speed = getRandomInt(100, 300);
+        this.sprite = 'images/enemy-bug.png';
+    }
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+    update(dt) {
+        if (this.x > 600) {
+            this.x = getRandomInt(-300, -100);
+            this.y = enemyLanes[getRandomInt(0, enemyLanes.length)];
+            this.speed = getRandomInt(100, 300);
+        } else {
+            this.x += dt * this.speed;
+        }
+    }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+class Player {
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+    constructor() {
+        this.x = 200;
+        this.y = 404;
+        this.sprite = 'images/char-boy.png';
+    }
 
+    update() { 
+    }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
 
+    handleInput(key) {
+        switch (key) {
+            case 'left':
+                this.x -= 100;
+                break;
+            case 'up':
+                this.y -= 83;
+                break;
+            case 'right':
+                this.x += 100;
+                break;
+            case 'down':
+                this.y += 83;
+                break;
+        }
+    }
+}
 
+const enemyLanes = [-20, 62, 146, 230, 310, 400];
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+const allEnemies = (function () {
+    var numberOfEnemies = 3;
+    var enemies = [];
+    for (var i = 0; i < numberOfEnemies; i++) {
+        enemies.push(new Enemy());
+    }
+    return enemies;
+})();
+
+const player = new Player();
+
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
@@ -44,3 +78,11 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+// Function from MDN
+// min is inclusive and max is exclusive
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; 
+}
